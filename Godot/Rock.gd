825f0717d403bf8076b77d503ec5d137405df567
Croobat <tonyram9906@outlook.com>
2021-09-27 12:43:06 -0500

    Big fix

--- Godot/Rock.gd
+++ Godot/Rock.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite
 onready var animated_sprite : AnimatedSprite = get_node(".")
 onready var score_anterior
 onready var rock_hp = 11
+onready var flag = 0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,9 +15,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	damage_rock()
 	if rock_hp == 0 and Data.player["Player"]["score"] != score_anterior:	
-		Globals.camera.shake(100,.4,100)
+		Globals.camera.shake(200,.4,200)
 		pick_rock()
 	score_anterior = Data.player["Player"]["score"];
 
@@ -45,14 +45,20 @@ func random_int(min_value,max_value, inclusive_range = false):
 	var range_size = max_value - min_value
 	return (randi() % range_size) + min_value
 
-func damage_rock():
+func _on_Player_hit_rock():
 	if Data.player["Player"]["score"] != score_anterior and Data.player["Player"]["score"] != 0:
-		print(1)
 		rock_hp -= 1
 		var new_particle = load("res://Scenes/Player/Particles2D.tscn").instance()
 		add_child(new_particle)
 		new_particle.position = Vector2(-5,5)
+
 		var new_particle2 = load("res://Scenes/Player/Particles2D2.tscn").instance()
 		add_child(new_particle2)
 		new_particle2.position = Vector2(-5,5)
-		print(new_particle.emitting)
+		var rand1 = random_int(0,2)
+		var rand2 = random_int(0,2)
+		if flag % 2 == 0:
+			self.position += Vector2(rand1,rand2)
+		else:
+			self.position = Vector2(96,80)
+		flag += 1
--- Godot/Singletons/Data.gd
+++ Godot/Singletons/Data.gd
@@ -3,9 +3,25 @@ extends Node
 var player = {
 	"Player": {
 	"score": 0,
+	"mpc": 1, #mpc: Mine per click
 	"game_over": false,
 	"paused": false
 	}
 }
 
-
+var unidades = {
+	"Buddy": {
+		"costo": 1,
+		"mps": 1
+	},
+	
+	"Jackhammer": {
+		"costo": 5,
+		"mps": 10
+	},
+	
+	"Fabrica": {
+		"costo": 10,
+		"mps": 100
+	}
+}
