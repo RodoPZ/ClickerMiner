bc0424b001d205d040e838c79a2ee5f4203af5c6
RodoPZ <rpinedo2799@gmail.com>
2021-09-27 13:47:05 -0500

    Commit despues de el gran bug jeje

--- Godot/Scenes/Upgrades/Buildings.gd
+++ Godot/Scenes/Upgrades/Buildings.gd
@@ -12,15 +12,19 @@ func _physics_process(delta):
 
 func focus():
 	if Input.is_action_just_pressed("ui_open_upgrades_up"):
+		get_node("VBoxContainer/"+unidades[focus]+"/Label").hide()
 		focus-=1
 		if focus <0:
 			focus = maxim
 	elif Input.is_action_just_pressed("ui_close_upgrades_down"):
+		get_node("VBoxContainer/"+unidades[focus]+"/Label").hide()
 		focus += 1
 		if focus >maxim:
 			focus = 0	
 	if focused == "buildings":
 		get_node("VBoxContainer/"+unidades[focus]).grab_focus()
+		get_node("VBoxContainer/"+unidades[focus]+"/Label").show()
+		
 	if Input.is_action_just_pressed("ui_A"):
 		unidades(unidades[focus])
 
