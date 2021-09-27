75e6630c1c8cb4773242c349174a54fa7578b8f2
RodoPZ <rpinedo2799@gmail.com>
2021-09-27 02:52:57 -0500

    Revert "Menu v.3"
    This reverts commit f211bad3571abf3bc7d62403802c89523b6e7ca7.

--- Godot/Scenes/Upgrades/Buildings.gd
+++ Godot/Scenes/Upgrades/Buildings.gd
@@ -2,15 +2,15 @@ extends Node2D
 var focus = 0 
 var maxim = 0
 var focused = "None"
-var unidades = ["Buddy","Jackhammer","Fabrica"]
-signal unidades(value)
-
 func _ready():
+	get_node("VBoxContainer/TextureButton"+str(focus)).grab_focus()
 	maxim = get_node("VBoxContainer").get_child_count()-1
+
 func _physics_process(delta):
 	focus()
 
 func focus():
+	print(focused)
 	if Input.is_action_just_pressed("ui_open_upgrades_up"):
 		focus-=1
 		if focus <0:
@@ -20,12 +20,7 @@ func focus():
 		if focus >maxim:
 			focus = 0	
 	if focused == "buildings":
-		get_node("VBoxContainer/"+unidades[focus]).grab_focus()
-	if Input.is_action_just_pressed("ui_A"):
-		unidades(unidades[focus])
-
-func unidades(value):
-	emit_signal("unidades",value)
+		get_node("VBoxContainer/TextureButton"+str(focus)).grab_focus()
 
 func _on_Mainscene_focus(value):
 	focused = value
