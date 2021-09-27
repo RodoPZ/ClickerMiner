a3ae9cec2d413eb7fb48e48e17d2cb52a63ff36b
RodoPZ <34044966+RodoPZ@users.noreply.github.com>
2021-09-27 16:27:55 -0500

    Revert "Menusv.3"

--- Godot/Scenes/Upgrades/Upgrades.gd
+++ Godot/Scenes/Upgrades/Upgrades.gd
@@ -1,4 +1,12 @@
-extends "res://Scenes/Upgrades/Menuscript.gd"
+extends Node2D
+var focus = 0 
+var maxim = 0
+var focused = ""
+func _ready():
+	maxim = get_node("VBoxContainer").get_child_count()-1
+
+func _physics_process(_delta):
+	focus()
 
 func focus():
 	if Input.is_action_just_pressed("ui_open_buildings_left"):
@@ -12,23 +20,14 @@ func focus():
 		if focus >maxim:
 			focus = 0
 	if focused == "upgrades":
-		if Input.is_action_just_pressed("ui_open_buildings_left"):
-			get_node("VBoxContainer/"+unidades[focus]+"/Label").hide()
-			focus-=1
-			if focus <0:
-				focus = maxim
-		elif Input.is_action_just_pressed("ui_close_buildings_right"):
-			get_node("VBoxContainer/"+unidades[focus]+"/Label").hide()
-			focus += 1
-			if focus >maxim:
-				focus = 0	
-				
-		get_node("VBoxContainer/"+unidades[focus]).grab_focus()
-		get_node("VBoxContainer/"+unidades[focus]+"/Label").show()
-		if Input.is_action_just_pressed("ui_A"):
-			unidades(unidades[focus])
-		if focus == maxim:
-			get_node("VBoxContainer").set_position(Vector2(-4,16))
-		elif focus == 0:
-			get_node("VBoxContainer").set_position(Vector2(4,16))
+		get_node("VBoxContainer/TextureButton"+str(focus)).grab_focus()
+		
+	if focus == maxim:
+		get_node("VBoxContainer").set_position(Vector2(-4,16))
+	elif focus == 0:
+		get_node("VBoxContainer").set_position(Vector2(4,16))
 
+
+func _on_Mainscene_focus(value):
+	focused = value
+	return focused
